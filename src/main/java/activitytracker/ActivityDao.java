d6ba7cb91da16360a103173c13c177c2d310f7ca
Arpee78 <arpee78@gmail.com>
2023-02-20 13:13:21 +0100

    Architecture and integration testing, configure entities

---
+++ src/main/java/activitytracker/ActivityDao.java
@@ -0,0 +1,42 @@
+package activitytracker;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class ActivityDao {
+
+    private EntityManagerFactory entityManagerFactory;
+
+    public ActivityDao(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    public void saveActivity(Activity activity){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(activity);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    public List<Activity> listActivities(){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        return entityManager.createQuery("select a from Activity", Activity.class).getResultList();
+    }
+
+    public Activity findActivityById(long id){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        return entityManager.find(Activity.class, id);
+    }
+
+    public void deleteActivity(long id){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Activity activity = entityManager.find(Activity.class, id);
+        entityManager.getTransaction().begin();
+        entityManager.remove(activity);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+}
---
+++ src/test/java/activitytracker/ActivityDaoTest.java
@@ -0,0 +1,78 @@
+package activitytracker;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityDaoTest {
+
+    private EntityManagerFactory entityManagerFactory;
+    private ActivityDao activityDao;
+
+    @BeforeEach
+    void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("pu");
+        activityDao = new ActivityDao(entityManagerFactory);
+    }
+
+    @AfterEach
+    void close() {
+        entityManagerFactory.close();
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveActivity() {
+        Activity activity = new Activity(
+                LocalDateTime.of(2023,3,15,3,5), "Futás az erdőben", ActivityType.RUNNING);
+        activityDao.saveActivity(activity);
+        Activity expected = activityDao
+                .findActivityById(activity.getId());
+        assertEquals("Futás az erdőben", expected.getDescription());
+    }
+
+    @org.junit.jupiter.api.Test
+    void listActivities() {
+        Activity activity1 = new Activity(
+                LocalDateTime.of(2023,3,15,3,5), "Futás az erdőben", ActivityType.RUNNING);
+        Activity activity2 = new Activity(
+                LocalDateTime.of(2023,4,15,3,5), "Biciklitúra a tó körül", ActivityType.BIKING);
+        Activity activity3 = new Activity(
+                LocalDateTime.of(2023,5,15,3,5), "Túra a hegyekben", ActivityType.HIKING);
+        activityDao.saveActivity(activity1);
+        activityDao.saveActivity(activity2);
+        activityDao.saveActivity(activity3);
+        List<Activity> activities = activityDao.listActivities();
+        assertEquals(3, activities.size());
+        assertThat(activities)
+                .extracting(Activity::getDescription)
+                .contains("morning running", "hiking near Budapest", "evening running");
+    }
+
+    @org.junit.jupiter.api.Test
+    void findActivityById() {
+        Activity activity = new Activity(
+                LocalDateTime.of(2023,3,15,3,5), "Futás az erdőben", ActivityType.RUNNING);
+        activityDao.saveActivity(activity);
+        Activity expected = activityDao.findActivityById(activity.getId());
+        assertEquals("Futás az erdőben", expected.getDescription());
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteActivity() {
+        Activity activity = new Activity(
+                LocalDateTime.of(2023,3,15,3,5), "Futás az erdőben", ActivityType.RUNNING);
+        activityDao.saveActivity(activity);
+        activityDao.deleteActivity(activity.getId());
+        List<Activity> expected = activityDao.listActivities();
+
+        assertEquals(0, expected.size());
+    }
+}
